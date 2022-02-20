b58eab8dce17a8db98e215ff4f354f8f2fea2b0d
Jim <jimstacey.spb@gmail.com>
2022-02-20 16:25:32 +0300

    Initial

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+      title: `Kingfin Gatsby`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"]
+};
